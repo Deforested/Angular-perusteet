3a10f48b28f516800968de6cb959ec3b14f9d68e
Deforested <Teemuyritys@gmail.com>
2025-01-23 09:59:20 +0200

    Ts Task 1 done

---
+++ part1/task1ts.js
@@ -0,0 +1,4 @@
+function print(name, occupation, wage) {
+    return "Hey, ".concat(name, ". You are an ").concat(occupation, " and your hourly wage is ").concat(wage, " dollars.");
+}
+console.log(print('Sam', 'Engineer', 25));
---
+++ part1/task1ts.ts
@@ -0,0 +1,4 @@
+function print(name: string, occupation: string, wage: number) {
+  return `Hey, ${name}. You are an ${occupation} and your hourly wage is ${wage} dollars.`;
+}
+console.log(print('Sam', 'Engineer', 25));
